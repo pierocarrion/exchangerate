f5344b78c81a310937cad68abf3a07692857ecdd
pierocarrion <piero.january15@gmail.com>
2023-01-22 16:23:38 -0500

    Router

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
         path: "/",
         element: <Exchange />,
     },
-]);
+], { basename: process.env.REACT_APP_BASEURL });
 
 export default function Home() {
     return <RouterProvider router={router} />
