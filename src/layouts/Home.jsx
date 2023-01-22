8ad7f388b5a6632b2448340063d48968727ea83d
pierocarrion <piero.january15@gmail.com>
2023-01-22 13:13:42 -0500

    Fixed routing

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -10,8 +10,8 @@ const router = createBrowserRouter([
         path: "/",
         element: <Exchange/>,
     },
-]);
+], { basename: process.env.REACT_APP_BASEURL ?? 'https://pierocarrion.github.io/exchangerate'});
 
 export default function Home() {
-    return <RouterProvider router={router} />
+    return <RouterProvider base router={router} />
 }
